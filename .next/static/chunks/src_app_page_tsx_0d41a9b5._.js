8f90b196c342372b5909051208086a804f6d9372
Tofi <tofi@archis.company>
2025-07-13 19:09:32 -0400

    [MO-5] About Section Oluşturma

--- .next/static/chunks/src_app_page_tsx_0d41a9b5._.js
+++ .next/static/chunks/src_app_page_tsx_0d41a9b5._.js
@@ -1,14 +1,14 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/node_modules_next_dist_ea69acce._.js",
+  "static/chunks/node_modules_next_74a8dda6._.js",
   "static/chunks/node_modules_three_build_three_core_df0bde7e.js",
   "static/chunks/node_modules_three_build_three_module_230a9f89.js",
   "static/chunks/node_modules_three_build_three_module_90b4155f.js",
   "static/chunks/node_modules_react-reconciler_25c8ec02._.js",
   "static/chunks/node_modules_@react-three_fiber_dist_8df35c1f._.js",
-  "static/chunks/node_modules_7e9e3d88._.js",
-  "static/chunks/src_components_1d0f5c61._.js"
+  "static/chunks/node_modules_6b1d5ecd._.js",
+  "static/chunks/src_components_945f3fac._.js"
 ],
     source: "dynamic"
 });
